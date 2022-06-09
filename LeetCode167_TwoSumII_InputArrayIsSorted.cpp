e000a9efc808e31d65d81f341d4181a3e734f709
vivek <95127627+vivek-panchal@users.noreply.github.com>
2022-06-09 10:54:43 +0530

    Create LeetCode167_TwoSumII_InputArrayIsSorted.cpp

---
+++ LeetCode167_TwoSumII_InputArrayIsSorted.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector<int> ans;
+        int n=numbers.size();
+        int i=0;
+        int j= n-1;
+        while(i<j){
+            if (numbers[i]+numbers[j]==target){
+                ans.push_back(i+1);
+                ans.push_back(j+1);
+                break;
+            }
+            if(numbers[i]+ numbers[j]>target){
+                j--;
+            }
+            if(numbers[i]+ numbers[j]<target){
+                i++;
+            }
+        }
+        return ans;
+    }
+};
