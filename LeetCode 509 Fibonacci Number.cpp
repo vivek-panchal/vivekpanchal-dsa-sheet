af5144e128affd89cc52da8eb46199e9da2c3432
vivek <95127627+vivek-panchal@users.noreply.github.com>
2022-06-13 16:24:45 +0530

    Create LeetCode 509 Fibonacci Number.cpp

---
+++ LeetCode 509 Fibonacci Number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int fib(int n) {
+        //base case
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        return fib(n-1) + fib(n-2);
+    }
+};
