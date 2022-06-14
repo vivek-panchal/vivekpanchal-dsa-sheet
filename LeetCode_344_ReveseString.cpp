7f25796e4c08be04aa007348d2b3aab1eb390d0f
vivek <95127627+vivek-panchal@users.noreply.github.com>
2022-06-14 12:04:31 +0530

    Update LeetCode_344_ReveseString.cpp

--- LeetCode_344_ReveseString.cpp
+++ LeetCode_344_ReveseString.cpp
@@ -8,3 +8,22 @@ public:
         }
     }
 };
+// recursive solution 
+class Solution {
+   void reverse(vector<char>& s , int i, int j){
+        
+        //base case
+        if(i>j)
+            return;
+        swap(s[i],s[j]);
+            
+        i++;
+        j--;
+        reverse(s,i,j);
+    }
+public:
+    void reverseString(vector<char>& s) {
+        reverse(s,0,s.size()-1);
+            
+    }
+};
