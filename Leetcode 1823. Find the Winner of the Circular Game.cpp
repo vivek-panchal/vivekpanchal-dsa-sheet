f6152c2373825461621fb62b9fdad122cdfaa544
vivek <95127627+vivek-panchal@users.noreply.github.com>
2022-06-22 17:36:23 +0530

    Update Leetcode 1823. Find the Winner of the Circular Game.cpp

--- Leetcode 1823. Find the Winner of the Circular Game.cpp
+++ Leetcode 1823. Find the Winner of the Circular Game.cpp
@@ -1,4 +1,30 @@
 // It is a Josephus Problem
+// easy to understand
+class Solution {
+private:
+    void solve(vector<int>v,int k,int index,int &ans){
+        if(v.size()==1){
+            ans=v[0];
+            return;
+        }
+        index=(index+k)%v.size();
+        v.erase(v.begin()+index);
+        solve(v,k,index,ans);
+    }
+public:
+    int findTheWinner(int n, int k) {
+        vector<int>v;
+        for(int i=1;i<=n;i++){
+            v.push_back(i);
+        }
+        k--;
+        int index=0;
+        int ans=-1;
+        solve(v,k,index,ans);
+        return ans;
+    }
+};
+//dificult to understand
 class Solution {
 public:
     int findTheWinner(int n, int k) {
