607e86363d280f649386c9280c45262446dcaf45
vivek <95127627+vivek-panchal@users.noreply.github.com>
2022-06-27 17:11:49 +0530

    Update LeetCode 206 Reverse a linkedList.cpp

--- LeetCode 206 Reverse a linkedList.cpp
+++ LeetCode 206 Reverse a linkedList.cpp
@@ -17,6 +17,32 @@ public:
     }
 };
 
+// love babbar recursive approach
+
+class Solution {
+private:
+    void reverse(ListNode* &head,ListNode* curr,ListNode* prev){
+        // base case 
+        if(curr==NULL){
+           // head ko previous per point kra do
+            head=prev;
+            return;
+        }
+        ListNode* temp=curr->next;
+        reverse(head,temp,curr);
+        curr->next=prev;
+    }
+public:
+    ListNode* reverseList(ListNode* head) {
+      // curr ko head per point kra diya   
+        ListNode* curr=head; 
+        // previous ko NULL per point kra do
+        ListNode* prev=NULL;
+        reverse(head,curr,prev);
+        
+        return head;
+    }
+};
 
 // recursive solution
 
