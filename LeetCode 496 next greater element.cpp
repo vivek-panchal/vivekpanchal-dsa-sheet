0159a0ae574899f7aee48d011b58212194279d19
vivek <95127627+vivek-panchal@users.noreply.github.com>
2022-07-07 14:52:16 +0530

    Update LeetCode 496 next greater element.cpp

--- LeetCode 496 next greater element.cpp
+++ LeetCode 496 next greater element.cpp
@@ -5,6 +5,7 @@ public:
         int m=nums1.size();
         unordered_map<int,int> umap;
         stack<int> st;
+        //nums 2 ke next greater element ko map me store kar lenge nums2[i] -> next greater element
         for(int i=n-1; i>=0;i--){
             int ele=nums2[i];
             while(!st.empty()&& st.top()<=ele){
@@ -14,6 +15,8 @@ public:
             umap.insert({ele,res});
             st.push(ele);
         }
+        
+        // nums1 ko traverse karenge and nums[i] ko map me dhundhenge and uske samne jo bhi value hai usse store kar lenge ans me 
         vector<int> ans;
         for(int i=0;i<m;i++){
             int x= nums1[i];
