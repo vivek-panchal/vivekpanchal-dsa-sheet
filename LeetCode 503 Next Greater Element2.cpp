df9189ec9e84f77a4035a23e0cb4f3784b899296
vivek <95127627+vivek-panchal@users.noreply.github.com>
2022-07-08 14:23:09 +0530

    Create LeetCode 503 Next Greater Element2.cpp

---
+++ LeetCode 503 Next Greater Element2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) 
+    {
+        int n = nums.size();
+        vector<int> res(n);
+        stack<int> st;
+        for(int i=2*n-1;i>=0;i--)
+        {
+            while(!st.empty() && nums[i%n]>=st.top())
+                    st.pop();
+            
+                if(st.empty()) res[i%n] = -1;
+                else res[i%n] = st.top();
+            
+            st.push(nums[i%n]);
+        }
+        return res;
+    }
+};
