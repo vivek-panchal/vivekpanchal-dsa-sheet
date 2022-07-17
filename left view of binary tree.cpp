4561c019434c51e48421ae6858cae99ae20b8379
vivek <95127627+vivek-panchal@users.noreply.github.com>
2022-07-17 14:10:49 +0530

    Create left view of binary tree.cpp

---
+++ left view of binary tree.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    // find left view of binary tree , save it into vector and return last element of this vector
+    void solve(TreeNode* root,vector<int> &ans,int lvl){
+        if(root==NULL)
+            return;
+        if(lvl==ans.size())
+            ans.push_back(root->val);
+        solve(root->left,ans,lvl+1);
+        solve(root->right,ans,lvl+1);
+    }
+    int findBottomLeftValue(TreeNode* root) {
+        vector<int> ans;
+        solve(root,ans,0);
+        
+        return ans;
+    }
+};
