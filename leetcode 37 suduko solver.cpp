0bc032c5dd5fdbb29c8cd2ae0ddb9e02c131be42
vivek <95127627+vivek-panchal@users.noreply.github.com>
2022-08-10 17:45:58 +0530

    Update leetcode 37 suduko solver.cpp

--- leetcode 37 suduko solver.cpp
+++ leetcode 37 suduko solver.cpp
@@ -3,9 +3,15 @@ class Solution {
 {
     for(int i=0;i<9;i++)
     {
-        if(board[row][i]==y) return false;
-        if(board[i][col]==y) return false;
-        if(board[3*(row/3)+i/3][3*(col/3)+i%3]==y) return false;
+        // check karo jha ham Y ko dal rahe hai kya uss row me Y phle se hai ya nhi 
+        if(board[row][i]==y)
+            return false;
+        // same check for col 
+        if(board[i][col]==y)
+            return false;
+        // check for 3X3 matrix
+        if(board[3*(row/3)+i/3][3*(col/3)+i%3]==y)
+            return false;
     }
     return true;
 }
@@ -15,18 +21,22 @@ bool solve(vector<vector<char>>&board)
     {
         for(int j=0;j<9;j++)
         {
+            // if we find any empty place , we try all possible numbers 1-9 at that place 
             if(board[i][j]=='.')
             {
                 for(int y='1';y<='9';y++)
-                {
+                {// we trying to put nuber from 1-9 , and check is this valid or not
                     if(valid(board,i,j,y))
                     {
                         board[i][j]=y;
+                        // y ko put karne ke baad check kar rahe hai kya aage solve ho pa rha hai if yes than return true &  if solution not found than backtrack and try other number
                         if(solve(board))
                         {
+                            //solution possible
                             return true;
                         }
                         else{
+                           // back track bcs if we put Y at that place we cant solve whole sudku
                             board[i][j]='.';
                         }
                     }
